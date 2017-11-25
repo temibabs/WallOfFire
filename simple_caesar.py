cc24b753e24e7d356786233fb2ae2300485e72c4
temibabs <temibabalola1.0@gmail.com>
2017-11-25 03:45:07 +0100

    Update simple_caesar.py

--- simple_caesar.py
+++ simple_caesar.py
@@ -2,6 +2,11 @@
 
 import random
 
-def cesar(plain_text):
-  key = random.randint(1, 26)
-  for 
+def cesar(plain_text, key):
+  #...using the ASCII range
+  # key = random.randint(1, 25)
+  cipher_text = ''
+  for i in plain_text:
+    cipher_text += ((ord(i) + key) if (ord(i) + key) <= 127 else (ord(i) + key) % 127)
+    
+# BE BACK LATER...
